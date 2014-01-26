9dcb841d70ea6c9c8621d80a6632011a5ad7cc20
Devitt Upkins <evilotaku@gmail.com>
2014-01-25 21:12:51 -0600

    got jumping working

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -7,7 +7,7 @@ public class playerController : MonoBehaviour {
     private GameObject player2;
     private GameObject player3;
     private GameObject player4;
-    private GameObject player5;
+   
 
     public float speed = 30.0f;
     public float jump = 10.0f;
@@ -23,27 +23,22 @@ public class playerController : MonoBehaviour {
         player2 = GameObject.Find("Player2");
         player3 = GameObject.Find("Player3");
         player4 = GameObject.Find("Player4");
-        player5 = GameObject.Find("Player5");
+       
 	
 	}
 	
 	// Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 1; i < 5; i++)
         {
-            if (Mathf.Abs(Input.GetAxis("Joy" + i + "X")) > 0.5
-                || Mathf.Abs(Input.GetAxis("Joy" + i + "Y")) > 0.5
-                || Mathf.Abs(Input.GetAxis("Xbox360Controller" + i + "RightX")) > 0.5)                
-            //Debug.Log(Input.GetJoystickNames()[i] + " is moved");
-            {
-
+           
                 switch (i)
                 {
-                    case 0:
+                    case 1:
                      player1.rigidbody.AddForce(player1.transform.forward * -Input.GetAxis("Joy" + i + "Y") * speed);                                      
                      player1.transform.Rotate(Vector3.up, Input.GetAxis("Xbox360Controller" + i + "RightX"));
-                     if (Input.GetButton("joystick 1 button 0"))
+                     if (Input.GetKeyDown("joystick 1 button 0"))
                          {
                              print("player 1 jumped");
                              player1.rigidbody.AddForce(player2.transform.up * jump, ForceMode.Impulse);
@@ -51,38 +46,33 @@ public class playerController : MonoBehaviour {
                      
                      break;
 
-                    case 1:
+                    case 2:
                      player2.rigidbody.AddForce(player2.transform.forward * -Input.GetAxis("Joy" + i + "Y") * speed);                                      
                      player2.transform.Rotate(Vector3.up, Input.GetAxis("Xbox360Controller" + i + "RightX"));
-                     if (Input.GetButton("joystick 2 button 0"))
+                     if (Input.GetKey("joystick 2 button 0"))
                      {
                          print("player 2 jumped");
                          player2.rigidbody.AddForce(player2.transform.up * jump, ForceMode.Impulse);
                      }  
                      break;
                     
-                    case 2:
+                    case 3:
                      player3.rigidbody.AddForce(player3.transform.forward * -Input.GetAxis("Joy" + i + "Y") * speed);                                      
                      player3.transform.Rotate(Vector3.up, Input.GetAxis("Xbox360Controller" + i + "RightX"));
                      if (Input.GetKey(KeyCode.Joystick3Button1)) player2.rigidbody.AddForce(player2.transform.up * jump, ForceMode.Impulse);
                      break;
 
-                    case 3:
+                    case 4:
                      player4.rigidbody.AddForce(player4.transform.forward * -Input.GetAxis("Joy" + i + "Y") * speed);                                      
                      player4.transform.Rotate(Vector3.up, Input.GetAxis("Xbox360Controller" + i + "RightX"));
                      if (Input.GetKey(KeyCode.Joystick4Button0)) player2.rigidbody.AddForce(player2.transform.up * jump, ForceMode.Impulse);
                      break;
 
-                    case 4:
-                     player5.rigidbody.AddForce(player5.transform.forward * -Input.GetAxis("Joy" + i + "Y") * speed);                                      
-                     player5.transform.Rotate(Vector3.up, Input.GetAxis("Xbox360Controller" + i + "RightX"));
-                     //if (Input.GetKey(KeyCode.Joystick5Button0)) player2.rigidbody.AddForce(player2.transform.up * jump, ForceMode.Impulse);
-                     break;
-
+                    
                 }
 
                
-            }
+            
 
         }
     }
